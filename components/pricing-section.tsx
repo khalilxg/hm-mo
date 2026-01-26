e22ca93e46ce8cc0e6f9ce41e508032947b3dd83
Med khalil bousnina <mkbousnina@gmail.com>
2026-01-26 13:10:08 +0100

    Update pricing value in pricing-section component

--- components/pricing-section.tsx
+++ components/pricing-section.tsx
@@ -283,7 +283,7 @@ export function PricingSection() {
                       dir="ltr"
                       className="text-3xl font-mono font-bold text-white tracking-wider"
                     >
-                      7623582
+                      53390171
                     </p>
                   </div>
 
